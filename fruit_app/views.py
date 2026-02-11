3d628ee80f1272efe77a9fb12060a574f9698d58
Jermaine Jérôme Bärwolf <jay.jero1901@gmail.com>
2026-02-11 13:42:42 +0100

    Implement send_fruits view to return a list of fruits as JSON

--- fruit_app/views.py
+++ fruit_app/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+def send_fruits(request):
+    fruits = [
+        {
+            "name": "Apfel", 
+            "gewicht": 150,
+            "farbe": "Rot"
+        },
+        {
+            "name": "Banane", 
+            "gewicht": 120,
+            "farbe": "Gelb"
+        },
+        {
+            "name": "Blaubeere", 
+            "gewicht": 2,
+            "farbe": "Blau"
+        },
+        {
+            "name": "Mango", 
+            "gewicht": 300,
+            "farbe": "Orange"
+        },
+        {
+            "name": "Kiwi", 
+            "gewicht": 70,
+            "farbe": "Grün"
+        }
+    ]
+    return JsonResponse(fruits, safe=False)
